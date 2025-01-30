e3b051b7f50e5ba3f4cd54012042386b328483e8
Kevin Wu <KWu@lcs.on.ca>
2025-01-30 15:17:18 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Yixuan Wu on 2025-01-30.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
